b65fc4e34c1b4f0be02530fde3bf9bc0f5765b79
Chris Allen <chrisjallen@me.com>
2013-08-10 19:41:11 +0100

    added view with transparent background full screen

--- birthday/RPAppDelegate.m
+++ birthday/RPAppDelegate.m
@@ -18,6 +18,23 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+	NSWindow * window = self.window;
+	[window setOpaque:NO];
+	NSColor * transparent = [NSColor colorWithCalibratedWhite:1.0 alpha:0.0];
+	[window setBackgroundColor:transparent];
+
+	//set max screen
+	NSScreen * screen = [NSScreen mainScreen];
+	CGRect frame = screen.frame;
+	[window setFrame:frame display:YES];
+	
+	//put at front
+	//[window orderFrontRegardless];
+	//[NSApp activateIgnoringOtherApps:YES];
+	
+	[window setLevel:NSScreenSaverWindowLevel + 1];
+	[window orderFront:nil];
+	
 }
 
 @end
